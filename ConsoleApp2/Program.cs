dbe5e6cd721b3ca731b94571deb85642b2e5bb62
79176 <79176@LAPTOP-MN0I58DS>
2023-09-07 19:34:13 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите угол в радианах");
+            int y = Convert.ToInt32(Console.ReadLine());
+            double cosAngle = Math.Cos(y);
+            Console.WriteLine("Косинус угла:"+cosAngle);
+
+        }
+    }
+}
